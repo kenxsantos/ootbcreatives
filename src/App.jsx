039c5056c4831a14a1976e44962fc02985740d23
kenxsantos <133564965+kenxsantos@users.noreply.github.com>
2024-07-09 17:01:46 +0800

    adjust landing page view

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,8 @@ import NavBar from "./components/NavBar";
 import Index from "./pages/Index";
 import { Routes, Route } from "react-router-dom";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <NavBar />
       <Routes>
         <Route path="/" element={<Index />} />
       </Routes>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,12 +5,11 @@ const NavBar = () => {
     <nav className="max-w-screen-2xl px-20">
       <div className="text-white flex justify-evenly items-center font-jost text-md">
         <section>SERVICES</section>
-        <section>SERVICES</section>
         <section className="flex flex-col leading-none">
           <span>CLIENTS</span>
           <span>ONBOARD</span>
         </section>
-        <section>CREWWMATES</section>
+        <section>CREWMATES</section>
         <section className="flex flex-col leading-none">
           <span>OOTB</span>
           <span>ACADEMY</span>
