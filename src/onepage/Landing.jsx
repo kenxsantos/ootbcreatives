d337c28d2b3096eceeba0e507b590e77281defa2
Ken Santos <kenxsantos@gmail.com>
2024-07-12 20:26:37 +0800

    added image overlay

--- src/onepage/Landing.jsx
+++ src/onepage/Landing.jsx
@@ -1,9 +1,13 @@
 import ReadMoreReadLess from "../components/ReadMoreReadLess";
-
 const Landing = () => {
   return (
-    <div className="bg-cover bg-indexbg">
-      <div className="max-w-7xl mx-auto ">
+    <div className="relative bg-cover bg-indexbg">
+      <img
+        src="../../public/assets/ShadowOverlay.png"
+        alt=""
+        className="absolute inset-0 w-full h-full object-cover z-0"
+      />
+      <div className="relative max-w-7xl mx-auto z-10">
         <div>
           <section className="mb-4">
             <h1 className="pt-[20rem] text-left text-[65px] leading-none text-white font-metropolis font-bold tracking-tighter">
