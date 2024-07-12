6d97bbdaee3f6eca3f4e9e133ac3b35c8ca10466
Ken Santos <kenxsantos@gmail.com>
2024-07-12 20:26:25 +0800

    adjust logo position

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,21 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { useAnimation, motion } from "framer-motion";
 const NavBar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
   const logoAnimation = useAnimation();
-
+  const [activeLink, setActiveLink] = useState(null);
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 70) {
+      if (window.scrollY > 50) {
         setIsScrolled(true);
         logoAnimation.start({
-          scale: 0.3,
+          scale: 0.5,
           opacity: 1,
-          x: -65,
-          y: -190,
+          x: -40,
+          y: -170,
           // Move the logo 50 pixels up
-          transition: { duration: 0.5 },
+          transition: { duration: 0.3 },
         });
       } else {
         setIsScrolled(false);
@@ -34,31 +35,45 @@ const NavBar = () => {
     };
   }, [logoAnimation]);
 
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+  };
+  const navLinks = [
+    { name: "SERVICES", id: "SERVICES" },
+    { name: "CLIENTS", id: "CLIENTS", sub: "ONBOARD" },
+    { name: "CREWMATES", id: "CREWMATES" },
+    { name: "OOTB", id: "OOTBACADEMY", sub: "ACADEMY" },
+    { name: "CAREERS", id: "CAREERS" },
+    { name: "RESOURCES", id: "RESOURCES" },
+    { name: "RADIO US", id: "RADIO US" },
+  ];
   return (
-    <nav className="w-full fixed top-0 z-50 pt-8">
-      <div className="mx-auto max-w-7xl  ">
-        <div className="text-white flex justify-evenly items-center font-jost text-md">
-          <section>SERVICES</section>
-          <section className="flex flex-col leading-none">
-            <span>CLIENTS</span>
-            <span>ONBOARD</span>
-            <img
-              src="/public/assets/flare.png"
-              alt=""
-              srcset=""
-              className="w-44 absolute left-[345px] top-[35px]"
-            />
-          </section>
-          <section>CREWMATES</section>
-          <section className="flex flex-col leading-none">
-            <span>OOTB</span>
-            <span>ACADEMY</span>
-          </section>
-          <section>CAREERS</section>
-          <section>RESOURCES</section>
-          <section>RADIO US</section>
+    <nav className="w-full fixed top-0 z-50 pt-4">
+      <div className="mx-auto max-w-7xl">
+        <div className="text-white flex items-center justify-evenly font-jost text-md px-24">
+          {navLinks.map((link) => (
+            <Link key={link.id} to="#" onClick={() => handleLinkClick(link.id)}>
+              <section className="flex flex-col items-center justify-center h-24 w-[150px]">
+                <section className="flex flex-col leading-none items-center">
+                  <span>{link.name}</span>
+                  {link.sub && <span>{link.sub}</span>}
+                </section>
+                {activeLink === link.id && (
+                  <motion.img
+                    src="/public/assets/lineflare.png"
+                    alt="flare"
+                    className="w-44 ml-2 absolute mt-16"
+                    initial={{ opacity: 0, y: 10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: 10 }}
+                    transition={{ duration: 0.3 }}
+                  />
+                )}
+              </section>
+            </Link>
+          ))}
         </div>
-        <div className="mt-20">
+        <div className="mt-8">
           <motion.section
             className="w-48 h-48"
             animate={logoAnimation}
