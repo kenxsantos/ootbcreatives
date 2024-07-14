f5cc0e650f67e3aaee3bd0bf8d26d3a099688b92
Ken Santos <kenxsantos@gmail.com>
2024-07-14 12:51:08 +0800

    adjust layout

--- src/onepage/Landing.jsx
+++ src/onepage/Landing.jsx
@@ -3,9 +3,11 @@ import React, { useState, useEffect, useRef } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 const Landing = () => {
   const [showVideo, setShowVideo] = useState(false);
+  const [bgClass, setBgClass] = useState("bg-indexbg");
 
   useEffect(() => {
     const timer = setTimeout(() => {
+      setBgClass("bg-black");
       setShowVideo(true);
     }, 5000);
 
@@ -13,46 +15,9 @@ const Landing = () => {
   }, []);
 
   return (
-    <div className="relative bg-cover bg-indexbg">
-      <AnimatePresence>
-        {!showVideo ? (
-          <motion.div
-            key="image"
-            initial={{ opacity: 1 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.2 }}
-            className="absolute inset-0 w-full h-full"
-          >
-            <img
-              src="/assets/ShadowOverlay2.png"
-              alt="Shadow Overlay"
-              className="absolute inset-0 w-full h-full object-cover z-10"
-            />
-          </motion.div>
-        ) : (
-          <motion.video
-            key="video"
-            src="/assets/OOTBREEL2021.mp4"
-            autoPlay
-            loop
-            muted
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.2 }}
-            className="absolute inset-0 w-full h-full object-cover z-0"
-          />
-        )}
-      </AnimatePresence>
-      <div className="absolute inset-0 w-full h-full object-cover z-10 pointer-events-none">
-        <img
-          src="assets/ShadowOverlay2.png"
-          alt="Shadow Overlay"
-          className="w-full h-full object-cover"
-        />
-      </div>
-      <div className="relative max-w-7xl mx-auto z-20">
-        <div>
+    <div className={`relative max-w-screen-2xl mx-auto bg-cover ${bgClass}`}>
+      <div className="relative max-w-screen-2xl mx-auto z-20 p-12">
+        <div className="relative z-30">
           <section className="mb-4">
             <h1 className="pt-[20rem] text-left text-[65px] leading-none text-white font-metropolis font-bold tracking-tighter">
               <span>BOLD IDEAS</span>
@@ -64,6 +29,43 @@ const Landing = () => {
             <ReadMoreReadLess />
           </section>
         </div>
+        <AnimatePresence>
+          {showVideo ? (
+            <motion.video
+              key="video"
+              src="/assets/OOTBREEL2021.mp4"
+              autoPlay
+              loop
+              muted
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+              className="absolute inset-0 w-full h-full object-cover z-0"
+            />
+          ) : (
+            <motion.img
+              key="overlay2"
+              src="/assets/ShadowOverlay2.png"
+              alt="Shadow Overlay"
+              initial={{ opacity: 1 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.5 }}
+              className="absolute inset-0 w-full h-full object-cover z-10"
+            />
+          )}
+        </AnimatePresence>
+        {showVideo && (
+          <motion.img
+            key="overlay3"
+            src="/assets/ShadowOverlay3.png"
+            alt="Shadow Overlay"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className="absolute inset-0 w-full h-full object-cover z-10"
+          />
+        )}
       </div>
     </div>
   );
