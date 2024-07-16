c110ce841db25a3dc55c0ebce4c9853f93e387f4
kenxsantos <133564965+kenxsantos@users.noreply.github.com>
2024-07-16 18:41:12 +0800

    modified navbar

--- src/components/FixedNavBar.jsx
+++ src/components/FixedNavBar.jsx
@@ -2,38 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useAnimation, motion } from "framer-motion";
 const NavBar = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
   const logoAnimation = useAnimation();
   const [activeLink, setActiveLink] = useState(null);
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 5) {
-        setIsScrolled(true);
-        logoAnimation.start({
-          scale: 0.5,
-          opacity: 1,
-          x: -40,
-          y: -170,
-          // Move the logo 50 pixels up
-          transition: { duration: 0.3 },
-        });
-      } else {
-        setIsScrolled(false);
-        logoAnimation.start({
-          scale: 1,
-          opacity: 1,
-          x: 0,
-          y: 0, // Reset the position
-          transition: { duration: 0.5 },
-        });
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [logoAnimation]);
 
   const handleLinkClick = (link) => {
     setActiveLink(link);
@@ -48,9 +18,18 @@ const NavBar = () => {
     { name: "RADIO US", id: "RADIO US" },
   ];
   return (
-    <nav className="w-full fixed top-0 z-50 ">
-      <div className="mx-auto max-w-screen-2xl px-12 pt-4">
-        <div className="text-white flex items-center justify-evenly font-jost text-md px-24">
+    <nav className="w-full top-0 z-50 ">
+      <div className="mx-auto max-w-screen-2xl px-16 pt-4">
+        <div className="text-white flex items-center font-jost text-md">
+          <div>
+            <Link to="/">
+              <img
+                src="/assets/OOTBLogoWhite.png"
+                alt="logo"
+                className="w-24 mr-16"
+              />
+            </Link>
+          </div>
           {navLinks.map((link) => (
             <Link key={link.id} to="#" onClick={() => handleLinkClick(link.id)}>
               <section className="flex flex-col items-center justify-center h-24 w-[150px]">
@@ -58,6 +37,7 @@ const NavBar = () => {
                   <span className="transition-all duration-300 ease-in-out  hover:text-glow hover:cursor-pointer">
                     {link.name}
                   </span>
+                  {link.sub && <span>{link.sub}</span>}
                 </section>
                 {activeLink === link.id && (
                   <motion.img
@@ -73,22 +53,6 @@ const NavBar = () => {
               </section>
             </Link>
           ))}
-        </div>
-        <div className="mt-8">
-          <motion.section
-            className="w-48 h-48"
-            animate={logoAnimation}
-            initial={{ scale: 1, opacity: 1 }}
-            transition={{ duration: 0.3 }}
-          >
-            <Link to="/">
-              <img
-                src="/assets/OOTBLogoWhite.png"
-                alt="logo"
-                className="w-24 mr-16"
-              />
-            </Link>
-          </motion.section>
         </div>
       </div>
     </nav>
