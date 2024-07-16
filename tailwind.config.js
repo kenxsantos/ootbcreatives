52275549d851693962848bffabf66bf3ddfff697
kenxsantos <133564965+kenxsantos@users.noreply.github.com>
2024-07-16 18:43:08 +0800

    add custom utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,47 @@ export default {
         "events-management": "linear-gradient(135deg, #8b2a18, #b45d27)",
         "public-relation": "linear-gradient(135deg, #3f2987, #903da4)",
       },
+      colors: {
+        orange: "#F38920",
+        purple: "#A667D1",
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        ".list-decimal-none": {
+          listStyleType: "none",
+          counterReset: "list",
+        },
+        ".list-decimal-none li": {
+          counterIncrement: "list",
+          position: "relative",
+          paddingLeft: "1.5em",
+        },
+        ".list-decimal-none li::before": {
+          content: 'counter(list) "."',
+          position: "absolute",
+          left: 0,
+          color: "white",
+        },
+        ".hide-scrollbar": {
+          /* For Firefox */
+          "scrollbar-width": "none",
+          /* For Internet Explorer and Edge */
+          "-ms-overflow-style": "none",
+        },
+        ".hide-scrollbar::-webkit-scrollbar": {
+          /* For WebKit-based browsers (Chrome, Safari) */
+          display: "none",
+        },
+        ".text-glow": {
+          "text-shadow": "0 0 10px rgba(255, 255, 255, 0.7)",
+        },
+        ".hover\\:text-glow": {
+          "@apply hover:text-glow": {},
+        },
+      });
+    },
+  ],
 };
