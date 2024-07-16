07562fe2f9496128fd8f35d961e4eeb2e7e6e7ff
kenxsantos <133564965+kenxsantos@users.noreply.github.com>
2024-07-16 18:40:41 +0800

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar.jsx";
 import Index from "./onepage/Index.jsx";
 import { Routes, Route } from "react-router-dom";
+import ShowServices from "./pages/ShowServices.jsx";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="services/:slug" element={<ShowServices />} />
       </Routes>
     </>
   );
