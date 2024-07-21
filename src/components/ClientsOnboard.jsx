2f05dcc6fa08fbeaf8d3073048e63374fbeb8a3d
kenxsantos <133564965+kenxsantos@users.noreply.github.com>
2024-07-21 11:09:51 +0800

    added clients modal

--- src/components/ClientsOnboard.jsx
+++ src/components/ClientsOnboard.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import clients from "../json/clients.json";
+import { useState } from "react";
+import ClientsModal from "./ClientsModal";
 const ClientsOnboard = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalContent, setModalContent] = useState({});
+
+  const openModal = (item) => {
+    setModalContent(item);
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
   return (
     <div>
-      <div className="font-jost text-white text-xl">CLIENTS ONBOARD</div>
+      <div className="font-jost text-white text-base">CLIENTS ONBOARD</div>
       <div className="w-full mt-4">
         {clients.map((client, index) => (
           <div key={index} className="flex gap-4">
@@ -12,8 +25,9 @@ const ClientsOnboard = () => {
                 const clientsOnboardItem = client.clientsOnboard[key];
                 return (
                   <div
-                    className="w-[270px] h-36 rounded-2xl bg-white shadow-inner-clients p-8 flex items-center"
+                    className="w-[270px] h-36 rounded-2xl bg-white shadow-inner-clients p-8 flex items-center hover:cursor-pointer"
                     key={idx}
+                    onClick={() => openModal(clientsOnboardItem)}
                   >
                     {clientsOnboardItem.logo && (
                       <img
@@ -29,6 +43,11 @@ const ClientsOnboard = () => {
           </div>
         ))}
       </div>
+      <ClientsModal
+        isOpen={isOpen}
+        close={closeModal}
+        modalContent={modalContent}
+      />
     </div>
   );
 };
--- src/components/RecentlyAdded.jsx
+++ src/components/RecentlyAdded.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import clients from "../json/clients.json";
+import ClientsModal from "./ClientsModal";
+import { useState } from "react";
 const RecentlyAdded = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalContent, setModalContent] = useState({});
+
+  const openModal = (item) => {
+    setModalContent(item);
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div>
-      <div className="font-jost text-white text-xl">RECENTLY ADDED</div>
+      <div className="font-jost text-white text-base">RECENTLY ADDED</div>
       <div className="w-full mt-4">
         {clients.map((client, index) => (
           <div key={index} className="flex gap-4">
@@ -12,8 +26,9 @@ const RecentlyAdded = () => {
                 const recentItem = client.recently[key];
                 return (
                   <div
-                    className="relative w-[270px] h-36 rounded-2xl bg-white shadow-inner-clients p-4 flex items-center"
+                    className="relative w-[270px] h-36 rounded-2xl bg-white shadow-inner-clients p-4 flex items-center hover:cursor-pointer"
                     key={idx}
+                    onClick={() => openModal(recentItem)}
                   >
                     <div className="bg-orange bg-[#F38920] rounded-full bottom-0 absolute w-full -ml-4 border-4 border-[#F38920] -mb-2 text-center text-white font-jost text-sm">
                       RECENTLY ADDED
@@ -32,6 +47,11 @@ const RecentlyAdded = () => {
           </div>
         ))}
       </div>
+      <ClientsModal
+        isOpen={isOpen}
+        close={closeModal}
+        modalContent={modalContent}
+      />
     </div>
   );
 };
