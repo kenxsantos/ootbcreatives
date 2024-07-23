217e5d735117cd49fcbc2def72637a4fbe1791bb
kenxsantos <133564965+kenxsantos@users.noreply.github.com>
2024-07-23 18:32:40 +0800

    show all components

--- src/onepage/Index.jsx
+++ src/onepage/Index.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Landing from "./Landing";
 import NavBar from "../components/NavBar";
 import Services from "./Services";
+import Clients from "./Clients";
 
 const Index = () => {
   return (
     <div>
       <NavBar />
-      {/* <Landing /> */}
+      <Landing />
       <Services />
+      <Clients />
     </div>
   );
 };
