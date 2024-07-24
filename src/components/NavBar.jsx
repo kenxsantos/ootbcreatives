abd4eb2cc058e2b9eb5b9b4639eb22f0776bc128
kenxsantos <133564965+kenxsantos@users.noreply.github.com>
2024-07-24 16:16:57 +0800

    modify navbar logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,38 +1,27 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { useAnimation, motion } from "framer-motion";
+import { Link as ScrollLink } from "react-scroll";
 
 const NavBar = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
-  const [isScrolling, setIsScrolling] = useState(false);
-  const logoAnimation = useAnimation();
   const blurAnimation = useAnimation();
-  const [activeLink, setActiveLink] = useState(null);
+  const [activeLink, setActiveLink] = useState("landing");
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  const navLinks = [
+    { id: "landing" },
+    { name: "SERVICES", id: "services" },
+    { name: "CLIENTS", id: "clients" },
+    { name: "CREWMATES", id: "crewmates" },
+    { name: "ACADEMY", id: "academy" },
+    { name: "CAREERS", id: "careers" },
+    { name: "RESOURCES", id: "resources" },
+    { name: "RADIO US", id: "radio" },
+  ];
 
   useEffect(() => {
     let scrollTimer = null;
 
     const handleScroll = () => {
-      if (window.scrollY > 5) {
-        setIsScrolled(true);
-        logoAnimation.start({
-          scale: 0.5,
-          opacity: 1,
-          x: -40,
-          y: -140,
-          transition: { duration: 0.1 },
-        });
-      } else {
-        setIsScrolled(false);
-        logoAnimation.start({
-          scale: 1,
-          opacity: 1,
-          x: 0,
-          y: 0,
-          transition: { duration: 0.5 },
-        });
-      }
-
       setIsScrolling(true);
       blurAnimation.start({
         backdropFilter: "blur(5px)",
@@ -55,68 +44,50 @@ const NavBar = () => {
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, [logoAnimation, blurAnimation]);
-
-  const handleLinkClick = (link) => {
-    setActiveLink(link);
-  };
-
-  const navLinks = [
-    { name: "SERVICES", id: "SERVICES" },
-    { name: "CLIENTS", id: "CLIENTS" },
-    { name: "CREWMATES", id: "CREWMATES" },
-    { name: "ACADEMY", id: "ACADEMY" },
-    { name: "CAREERS", id: "CAREERS" },
-    { name: "RESOURCES", id: "RESOURCES" },
-    { name: "RADIO US", id: "RADIO US" },
-  ];
+  }, [blurAnimation]);
 
   return (
-    <nav className="w-full fixed top-0 z-50 h-28 overflow-visible">
+    <nav className="w-full fixed top-0 z-40 h-28 overflow-visible">
       <motion.div
-        className="mx-auto max-w-screen-2xl px-12 pt-4 h-28"
+        className="mx-auto max-w-screen-2xl px-12 h-24"
         animate={blurAnimation}
         initial={{ backdropFilter: "blur(0px)" }}
       >
         <div className="text-white flex items-center justify-evenly font-jost text-md px-24">
           {navLinks.map((link) => (
-            <Link key={link.id} to="#" onClick={() => handleLinkClick(link.id)}>
-              <section className="flex flex-col items-center justify-center h-24 w-[150px]">
-                <section className="flex flex-col leading-none items-center">
-                  <span className="transition-all duration-300 ease-in-out hover:text-glow hover:cursor-pointer">
-                    {link.name}
-                  </span>
-                </section>
-                {activeLink === link.id && (
-                  <motion.img
-                    src="/assets/others/lineflare.png"
-                    alt="flare"
-                    className="w-44 ml-2 absolute mt-16"
-                    initial={{ opacity: 0, y: 10 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    exit={{ opacity: 0, y: 10 }}
-                    transition={{ duration: 0.3 }}
-                  />
-                )}
-              </section>
-            </Link>
+            <ScrollLink
+              key={link.id}
+              to={link.id}
+              spy={true}
+              smooth={true}
+              duration={800}
+              offset={0} // Adjust this offset according to your navbar height
+              onSetActive={() => setActiveLink(link.id)}
+              className="relative flex flex-col items-center justify-center h-24 w-[150px] cursor-pointer"
+            >
+              <motion.span
+                whileHover={{ scale: 1.1 }}
+                transition={{ type: "spring", stiffness: 400, damping: 10 }}
+                className="s hover:text-glow -ml-20"
+              >
+                {link.name}
+              </motion.span>
+              {activeLink === link.id && activeLink !== "landing" && (
+                <motion.img
+                  src="/assets/others/lineflare.png"
+                  alt="flare"
+                  className="w-44 absolute mt-12 -ml-20"
+                  initial={{ opacity: 0, scale: 0.5 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  transition={{
+                    duration: 0.8,
+                    delay: 0.5,
+                    ease: [0, 0.71, 0.2, 1.01],
+                  }}
+                />
+              )}
+            </ScrollLink>
           ))}
-        </div>
-        <div>
-          <motion.section
-            className="w-48 h-48"
-            animate={logoAnimation}
-            initial={{ scale: 1, opacity: 1 }}
-            transition={{ duration: 0.3 }}
-          >
-            <Link to="/">
-              <img
-                src="/assets/logo/OOTBLogoWhite.png"
-                alt="logo"
-                className="w-48 mr-16"
-              />
-            </Link>
-          </motion.section>
         </div>
       </motion.div>
     </nav>
