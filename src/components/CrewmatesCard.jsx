ec6cf4e917b9c5ac27702a4d4f234c8c18a6abdc
Ken Santos <kenxsantos@gmail.com>
2024-07-29 22:09:46 +0800

    modify the layouts and design

--- src/components/CrewmatesCard.jsx
+++ src/components/CrewmatesCard.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import ReadMoreButton from "./ReadMoreButton";
 import crewmates from "../json/crewmates.json";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -7,43 +8,48 @@ import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
+import { Mousewheel, Pagination, Navigation } from "swiper/modules";
 
 const CrewmatesCard = () => {
-  const [isExpanded, setIsExpanded] = useState(false);
+  const [activeCard, setActiveCard] = useState(0);
 
-  const handleToggle = () => {
-    setIsExpanded(!isExpanded);
+  const handleSlideChange = (swiper) => {
+    setActiveCard(swiper.realIndex);
   };
 
   return (
-    <div className="max-w-screen-2xl ">
+    <div className="max-w-screen-2xl mx-auto">
       <Swiper
         effect={"coverflow"}
         grabCursor={true}
         centeredSlides={true}
         loop={true}
-        slidesPerView={"auto"}
+        slidesPerView={3}
+        mousewheel={true}
         centerInsufficientSlides={true}
-        slideToClickedSlide={true}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 100,
-          modifier: 2.5,
-        }}
-        pagination={{ el: ".swiper-pagination", clickable: true }}
-        navigation={{
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
+        spaceBetween={0}
+        pagination={{
           clickable: true,
+          el: ".custom-pagination",
         }}
-        modules={[EffectCoverflow, Pagination, Navigation]}
-        className="h-full py-8 relative"
+        modules={[Mousewheel, Pagination, Navigation]}
+        className="h-full py-8 relative w-full py-20"
+        onSlideChange={handleSlideChange}
       >
         {crewmates.map((crewmate, index) => (
-          <SwiperSlide className="w-[25rem] h-full  mx-auto ">
-            <section key={index} className="rounded-2xl bg-purple px-6 py-8">
+          <SwiperSlide
+            key={index}
+            className="w-[35rem] h-full mx-auto flex justify-center bg-transparent"
+          >
+            <motion.section
+              className={`rounded-2xl px-6 py-8 ${
+                index === activeCard ? "bg-orange" : "bg-purple"
+              } `}
+              animate={{
+                scale: index === activeCard ? 1.25 : 1,
+                transition: { duration: 0.5, ease: "easeInOut" },
+              }}
+            >
               <div className="relative w-72 h-64">
                 <div className="flex w-full">
                   <h1 className="font-metropolis text-white font-bold text-3xl leading-none w-3/4">
@@ -61,11 +67,11 @@ const CrewmatesCard = () => {
                   <h1 className="font-jost text-white text-xs uppercase mb-2">
                     {crewmate.position}
                   </h1>
-                  <Link to={`/crewmates/${crewmate.link}`} key={index}>
-                    <ReadMoreButton
-                      isExpanded={isExpanded}
-                      handleToggle={handleToggle}
-                    />
+                  <Link
+                    to={`/crewmates/${crewmate.link}/${crewmate.id}`}
+                    key={index}
+                  >
+                    <ReadMoreButton />
                   </Link>
                   <h1 className="absolute bottom-0 text-xs font-jost text-white uppercase">
                     {crewmate.title}
@@ -79,26 +85,16 @@ const CrewmatesCard = () => {
                   />
                 </div>
               </div>
-            </section>
+            </motion.section>
           </SwiperSlide>
         ))}
-
-        <div className="relative bottom-8 flex items-center justify-center slider-controler mt-16">
-          <div className="swiper-button-prev slider-arrow bg-white w-[3.5rem] h-[3.5rem] rounded-full left-[30%] transform translate-x-[-30%] md:left-[20%] md:translate-x-[-20%] lg:left-[42%] lg:translate-x-[-42%]">
-            <ion-icon
-              name="arrow-back-outline border "
-              className="text-2xl text-[#222224]"
-            ></ion-icon>
-          </div>
-          <div className="swiper-button-next slider-arrow bg-white w-[3.5rem] h-[3.5rem] rounded-full left-[70%] transform translate-x-[-70%] md:left-[80%] md:translate-x-[-80%] lg:left-[58%] lg:translate-x-[-58%]">
-            <ion-icon
-              name="arrow-forward-outline border"
-              className="text-2xl text-[#222224]"
-            ></ion-icon>
-          </div>
-          <div className="swiper-pagination relative w-[15rem] bottom-4"></div>
-        </div>
       </Swiper>
+
+      <div className="relative bottom-8 flex items-center justify-center mt-16 ">
+        <div className="flex  justify-evenly gap-28 items-center justify-center">
+          <div className="custom-pagination flex justify-center w-full"></div>
+        </div>
+      </div>
     </div>
   );
 };
