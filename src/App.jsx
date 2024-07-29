25eacfde4a20d7e9e0d68970fa107e6ad8383cd3
Ken Santos <kenxsantos@gmail.com>
2024-07-29 22:08:38 +0800

    update routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Index from "./onepage/Index.jsx";
 import { Routes, Route } from "react-router-dom";
 import ShowServices from "./pages/ShowServices.jsx";
 import ShowCrewmates from "./pages/ShowCrewmates.jsx";
+import CrewmatesCard from "./components/CrewmatesCard.jsx";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="services/:slug" element={<ShowServices />} />
-        <Route path="crewmates/:slug" element={<ShowCrewmates />} />
+        <Route path="crewmates/:slug/:id" element={<ShowCrewmates />} />
       </Routes>
     </>
   );
