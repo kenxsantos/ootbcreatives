fb73a048faf465b3465894cc9bd26a6ee0e22be4
Ken Santos <kenxsantos@gmail.com>
2024-08-07 21:59:47 +0800

    modify dropdown button

--- src/components/DropdownButton.jsx
+++ src/components/DropdownButton.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { TiArrowSortedDown } from "react-icons/ti";
 import { motion } from "framer-motion";
-const DropdownButton = () => {
+const DropdownButton = ({ years, selectedYear, onSelectYear }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleDropdown = () => {
@@ -16,54 +16,32 @@ const DropdownButton = () => {
         className="flex gap-2 items-center justify-center border-white border text-white font-jost px-4 py-1 text-sm "
         type="button"
       >
-        2024
+        {selectedYear}
         <TiArrowSortedDown size={16} />
       </button>
       {isOpen && (
         <div
           id="dropdown"
-          className="absolute z-10 mt-2 backdrop-blur-sm bg-white/30  rounded-lg shadow w-full text-center"
+          className="absolute z-10 mt-2 backdrop-blur-sm bg-white/30 rounded-lg shadow w-full text-center"
         >
           <ul
             className="py-2 text-sm font-jost text-white"
             aria-labelledby="dropdownDefaultButton"
           >
-            <li>
-              <motion.a
-                whileHover={{ scale: 1.1, cursor: "pointer" }}
-                href="#"
-                className="block px-4 py-2 text-white hover:text-glow"
-              >
-                2023
-              </motion.a>
-            </li>
-            <li>
-              <motion.a
-                whileHover={{ scale: 1.1, cursor: "pointer" }}
-                href="#"
-                className="block px-4 py-2 text-white hover:text-glow"
-              >
-                2023
-              </motion.a>
-            </li>
-            <li>
-              <motion.a
-                whileHover={{ scale: 1.1, cursor: "pointer" }}
-                href="#"
-                className="block px-4 py-2 text-white hover:text-glow"
-              >
-                2023
-              </motion.a>
-            </li>
-            <li>
-              <motion.a
-                whileHover={{ scale: 1.1, cursor: "pointer" }}
-                href="#"
-                className="block px-4 py-2 text-white hover:text-glow"
-              >
-                2023
-              </motion.a>
-            </li>
+            {years.map((year) => (
+              <li key={year}>
+                <motion.a
+                  whileHover={{ scale: 1.1, cursor: "pointer" }}
+                  onClick={() => {
+                    onSelectYear(year);
+                    setIsOpen(false);
+                  }}
+                  className="block px-4 py-2 text-white hover:text-glow"
+                >
+                  {year}
+                </motion.a>
+              </li>
+            ))}
           </ul>
         </div>
       )}
