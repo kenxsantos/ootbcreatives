35eb7187d937f14771d54fcb130b8077cfccd31a
Ken Santos <kenxsantos@gmail.com>
2024-08-08 21:12:03 +0800

    adjust width

--- src/components/TransparentDiv.jsx
+++ src/components/TransparentDiv.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { motion } from "framer-motion";
-const TransparentDiv = ({ title, description, index }) => {
-  const width = 530 + index * 20;
+const TransparentDiv = ({ title, description, index, width }) => {
   return (
     <motion.div
       whileTap={{ scale: 1.1 }}
       whileHover={{ scale: 1.1, cursor: "pointer" }}
       transition={{ type: "spring", stiffness: 400, damping: 15 }}
-      className="shadow-inner-crewmates rounded-3xl flex flex-col justify-start h-60 -mb-16 w-[500px]"
-      style={{ width: `${width}px` }}
+      className="shadow-inner-crewmates rounded-3xl flex flex-col justify-start h-60 -mb-16 w-[600px] "
+      style={{ width: `${width + index * 20}px` }}
     >
       <div className="p-10">
         <h1 className="text-3xl font-extrabold mb-2 font-metropolis text-white text-glow uppercase">
