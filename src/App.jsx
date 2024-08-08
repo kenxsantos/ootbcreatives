e8d7b0804c2e38fca7c1973a088528c55eae17cd
Ken Santos <kenxsantos@gmail.com>
2024-08-08 21:11:31 +0800

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import NavBar from "./components/NavBar.jsx";
 import Index from "./onepage/Index.jsx";
 import { Routes, Route } from "react-router-dom";
 import ShowServices from "./pages/ShowServices.jsx";
 import ShowCrewmates from "./pages/ShowCrewmates.jsx";
-import CrewmatesCard from "./components/CrewmatesCard.jsx";
 import ShowAcademy from "./pages/ShowAcademy.jsx";
 import MeetTheIntern from "./pages/MeetTheInterns.jsx";
+import ShowBoardingPass from "./pages/ShowBoardingPass.jsx";
 function App() {
   return (
     <>
@@ -17,6 +16,10 @@ function App() {
         <Route
           path="/ootb/academy/meet-the-interns"
           element={<MeetTheIntern />}
+        />
+        <Route
+          path="/ootb/academy/meet-the-interns/:year/:batch/:intern"
+          element={<ShowBoardingPass />}
         />
       </Routes>
     </>
