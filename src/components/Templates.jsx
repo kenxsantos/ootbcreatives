1f7a47febfed3501251d7ee11a3dd376e28ba8ac
kenxsantos <133564965+kenxsantos@users.noreply.github.com>
2024-08-15 14:08:55 +0800

    add templates

--- src/components/Templates.jsx
+++ src/components/Templates.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import SearchForm from "./SearchForm";
 
 const Templates = () => {
-  return <div>Templates</div>;
+  return (
+    <div className="w-[90%] mx-auto mt-12  h-full flex flex-col gap-4 overflow-auto hide-scrollbar">
+      <div>
+        <SearchForm />
+      </div>
+      <div className=" w-full px-8 flex gap-4 overflow-auto hide-scrollbar items-center justify-center">
+        <div className="w-32 h-12 bg-gray-500 bg-opacity-40 rounded-md flex-shrink-0"></div>
+        <div className="w-32 h-12 bg-gray-500 bg-opacity-40 rounded-md flex-shrink-0"></div>
+        <div className="w-32 h-12 bg-gray-500 bg-opacity-40 rounded-md flex-shrink-0"></div>
+        <div className="w-32 h-12 bg-gray-500 bg-opacity-40 rounded-md flex-shrink-0"></div>
+        <div className="w-32 h-12 bg-gray-500 bg-opacity-40 rounded-md flex-shrink-0"></div>
+        <div className="w-32 h-12 bg-gray-500 bg-opacity-40 rounded-md flex-shrink-0"></div>
+        <div className="w-32 h-12 bg-gray-500 bg-opacity-40 rounded-md flex-shrink-0"></div>
+        <div className="w-32 h-12 bg-gray-500 bg-opacity-40 rounded-md flex-shrink-0"></div>
+        <div className="w-32 h-12 bg-gray-500 bg-opacity-40 rounded-md flex-shrink-0"></div>
+        <div className="w-32 h-12 bg-gray-500 bg-opacity-40 rounded-md flex-shrink-0"></div>
+      </div>
+      <div className="overflow-auto hide-scrollbar h-[310px] w-full  flex flex-col gap-4">
+        <div className=" w-full px-8 flex gap-4 items-center justify-center">
+          <div className="w-[400px] h-[300px] bg-gray-500 bg-opacity-40 rounded-md flex-shrink-0"></div>
+          <div className="w-[400px] h-[300px] bg-gray-500 bg-opacity-40 rounded-md flex-shrink-0"></div>
+          <div className="w-[400px] h-[300px] bg-gray-500 bg-opacity-40 rounded-md flex-shrink-0"></div>
+        </div>
+        <div className=" w-full px-8 flex gap-4 items-center justify-center">
+          <div className="w-[400px] h-[300px] bg-gray-500 bg-opacity-40 rounded-md flex-shrink-0"></div>
+          <div className="w-[400px] h-[300px] bg-gray-500 bg-opacity-40 rounded-md flex-shrink-0"></div>
+          <div className="w-[400px] h-[300px] bg-gray-500 bg-opacity-40 rounded-md flex-shrink-0"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Templates;
