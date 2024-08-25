f7309eaac832a3d5a75b946dccf93050107d824f
Ken Santos <kenxsantos@gmail.com>
2024-08-25 22:39:05 +0800

    add screen sizes breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ export default {
         lg: "1024px",
         xl: "1280px",
         "2xl": "1536px",
+        "3xl": "1920px",
+        "4xl": "2560px",
+        "5xl": "3200px",
       },
       fontFamily: {
         metropolis: ["Metropolis", "sans-serif"],
