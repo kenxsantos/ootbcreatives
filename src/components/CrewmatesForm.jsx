d327836e3a1172d323273a9a7230bb8d35c597de
Ken Santos <kenxsantos@gmail.com>
2024-09-11 22:15:00 +0800

    update form values

--- src/components/CrewmatesForm.jsx
+++ src/components/CrewmatesForm.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { MdEmail } from "react-icons/md";
+import {
+  MdEmail,
+  MdCall,
+  MdDateRange,
+  MdAccessTimeFilled,
+} from "react-icons/md";
 import { FaFileUpload } from "react-icons/fa";
 import emailjs from "emailjs-com";
 import { initializeApp } from "firebase/app";
@@ -26,7 +31,10 @@ const CrewmatesForm = ({ name, setName }) => {
 
   const [formValues, setFormValues] = useState({
     email: "",
+    contact: "",
     position: "",
+    date: "",
+    message: "",
     file: null,
   });
 
@@ -67,7 +75,10 @@ const CrewmatesForm = ({ name, setName }) => {
       const templateParams = {
         name: name,
         email: formValues.email,
+        contact: formValues.contact,
         position: formValues.position,
+        date: formValues.date,
+        message: formValues.message,
         fileName: fileName || "No file uploaded",
         fileURL: fileURL || "No file uploaded",
       };
@@ -81,7 +92,10 @@ const CrewmatesForm = ({ name, setName }) => {
       alert("Email successfully sent!");
       setFormValues({
         email: "",
+        contact: "",
         position: "",
+        date: "",
+        message: "",
         file: null,
       });
       setName("");
@@ -89,7 +103,7 @@ const CrewmatesForm = ({ name, setName }) => {
       alert("Failed to send email. Please try again.");
       console.error("Error sending email:", error);
     } finally {
-      setLoading(false); // End loading animation
+      setLoading(false);
     }
   };
 
@@ -111,7 +125,7 @@ const CrewmatesForm = ({ name, setName }) => {
           className="w-full"
           encType="multipart/form-data"
         >
-          <div className="w-full mb-4">
+          <div className="w-full mb-2">
             <div className="relative w-full min-w-[200px] xs:h-10 lg:h-12 flex items-center gap-2">
               <MdEmail size={24} color="white" />
               <input
@@ -125,8 +139,23 @@ const CrewmatesForm = ({ name, setName }) => {
               />
             </div>
           </div>
-
-          <div className="w-full mb-4">
+          <div className="w-full mb-2">
+            <div className="relative w-full min-w-[200px] xs:h-10 lg:h-12 flex items-center gap-2">
+              <MdCall size={24} color="white" />
+              <input
+                type="tel"
+                name="contact"
+                value={formValues.contact}
+                onChange={handleChange}
+                className="peer w-full h-full bg-transparent text-white font-jost text-base outline outline-0 focus:outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 border focus:border-2 px-3 py-2.5 rounded-sm border-blue-gray-200"
+                placeholder="CONTACT NUMBER"
+                pattern="[0-9]{11}"
+                required
+                maxLength="11"
+              />
+            </div>
+          </div>
+          <div className="w-full mb-2">
             <div className="relative w-full min-w-[200px] xs:h-10 lg:h-12 flex items-center gap-2">
               <img
                 src="/assets/logo/OOTBLogoRed.webp"
@@ -176,10 +205,52 @@ const CrewmatesForm = ({ name, setName }) => {
             </div>
           </div>
 
-          <div className="w-full mb-4">
+          <div className="w-full mb-8">
             <div className="relative w-full min-w-[200px] h-10 flex items-center gap-2">
-              <FaFileUpload size={24} color="white" />
-              <div className="grid w-full gap-1">
+              <MdDateRange size={24} color="white" className="mt-10" />
+              <div className="grid w-full gap-1 mt-6">
+                <label htmlFor="date" className="text-sm font-jost text-white">
+                  Select target start date
+                </label>
+                <input
+                  id="date"
+                  name="date"
+                  type="date"
+                  value={formValues.date}
+                  onChange={handleChange}
+                  className="bg-transparent font-jost border border-gray-300 text-white text-base rounded-sm focus:border-white focus:outline-none block w-full p-2 z-50"
+                  required
+                />
+              </div>
+            </div>
+          </div>
+          <div className="w-full mb-28">
+            <div className="relative w-full min-w-[200px] h-10 flex items-center gap-2">
+              <MdAccessTimeFilled size={24} color="white" className="mt-10" />
+              <div className="grid w-full gap-1 mt-28">
+                <label htmlFor="date" className="text-sm font-jost text-white">
+                  List 3 time slots for an interview
+                </label>
+                <textarea
+                  name="message"
+                  rows="4"
+                  value={formValues.message}
+                  onChange={handleChange}
+                  className="p-2.5 w-full text-white bg-transparent font-jost text-base rounded-sm border border-gray-300 focus:ring-white focus:border-white focus:outline-none"
+                  placeholder="Write your time availability here... (e.g. 9:00 AM - 10:00 AM)"
+                  required
+                ></textarea>
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full mb-8">
+            <div className="relative w-full min-w-[200px] h-10 flex items-center gap-2">
+              <FaFileUpload size={24} color="white" className="mt-12" />
+              <div className="grid w-full gap-1 mt-6">
+                <label htmlFor="date" className="text-sm font-jost text-white">
+                  Upload your resume
+                </label>
                 <input
                   id="file"
                   name="file"
