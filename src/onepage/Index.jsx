61fb7b47fef2d7708e61dda4d345bbfcfe3220f3
kenxsantos <133564965+kenxsantos@users.noreply.github.com>
2025-01-16 19:19:03 +0800

    replace with react-mega-scroll

--- src/onepage/Index.jsx
+++ src/onepage/Index.jsx
@@ -1,114 +1,42 @@
-import React, { useState, useEffect, useCallback } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import Landing from "./Landing";
+import React, { useState } from "react";
+import MegaScroll from "react-mega-scroll";
+
 import NavBar from "../components/NavBar";
-import Services from "./Services";
+import Landing from "./Landing";
+import Service from "./Service";
 import Clients from "./Clients";
 import Crewmates from "./Crewmates";
 import Offers from "./Offers";
 import RadioUs from "./RadioUs";
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import { Mousewheel, Pagination, Navigation } from "swiper/modules";
-import Service from "./Service";
 
 const Index = () => {
-  const [swiperInstance, setSwiperInstance] = useState(null);
-  const [activeLink, setActiveLink] = useState("landing");
-
-  const navLinks = [
-    { name: "LANDING", id: "landing", slideIndex: 0 },
-    { name: "SERVICES", id: "services", slideIndex: 1 },
-    { name: "CLIENTS", id: "clients", slideIndex: 2 },
-    { name: "CREWMATES", id: "crewmates", slideIndex: 3 },
-    { name: "ACADEMY", id: "academy", slideIndex: 4 },
-    { name: "RADIO US", id: "radio", slideIndex: 5 },
-  ];
-
-  const handleNavLinkClick = useCallback(
-    (slideIndex) => {
-      if (swiperInstance) {
-        swiperInstance.slideTo(slideIndex);
-      }
-    },
-    [swiperInstance]
-  );
-
-  useEffect(() => {
-    if (swiperInstance) {
-      const onSlideChange = () => {
-        const activeSlide = swiperInstance.activeIndex;
-        const activeNavLink = navLinks.find(
-          (link) => link.slideIndex === activeSlide
-        );
-        if (activeNavLink) {
-          setActiveLink(activeNavLink.id);
-        }
-      };
-
-      swiperInstance.on("slideChange", onSlideChange);
-
-      // Cleanup event listener when swiperInstance changes or component unmounts
-      return () => {
-        swiperInstance.off("slideChange", onSlideChange);
-      };
-    }
-  }, [swiperInstance, navLinks]);
-
+  const [active, setActive] = useState(0);
   return (
-    <div className="relative max-w-screen-3xl mx-auto">
-      <NavBar onNavLinkClick={handleNavLinkClick} activeLink={activeLink} />
-      <Swiper
-        onSwiper={setSwiperInstance}
-        direction={"vertical"}
-        slidesPerView={1}
-        mousewheel={{
-          forceToAxis: true,
-          releaseOnEdges: true,
-          thresholdDelta: 50,
-        }}
-        lazy="true"
-        spaceBetween={0}
-        modules={[Mousewheel, Pagination, Navigation]}
-        className="h-screen w-full"
-        speed={400}
-      >
-        <SwiperSlide>
-          <div id="landing" className="h-full w-full">
-            <Landing />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div id="services" className="h-full w-full relative z-30">
-            <Service />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div id="clients" className="h-full w-full relative z-40">
-            <Clients />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div id="crewmates" className="h-full w-full relative z-50">
-            <Crewmates />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div id="academy" className="h-full w-full relative z-[60]">
-            <div id="careers"></div>
-            <div id="resources"></div>
-            <Offers />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div id="radio" className="h-full w-full relative z-[70]">
-            <RadioUs />
-          </div>
-        </SwiperSlide>
-      </Swiper>
-    </div>
+    <>
+      <NavBar />
+      <MegaScroll onChange={setActive}>
+        <div id="landing" className="h-full w-full">
+          <Landing />
+        </div>
+        <div id="services" className="h-full w-full relative z-30">
+          <Service />
+        </div>
+        <div id="clients" className="h-full w-full relative z-40">
+          <Clients />
+        </div>
+        <div id="crewmates" className="h-full w-full relative z-50">
+          <Crewmates />
+        </div>
+        <div id="academy" className="h-full w-full relative z-[60]">
+          <div id="careers"></div>
+          <div id="resources"></div>
+          <Offers />
+        </div>
+        <div id="radio" className="h-full w-full relative z-[70]">
+          <RadioUs />
+        </div>
+      </MegaScroll>
+    </>
   );
 };
 
